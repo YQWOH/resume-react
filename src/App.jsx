23a2c1623f6b0a81f352c91834482bbb30588a55
Steven Woh <wohynqin@gmail.com>
2023-09-17 23:31:37 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function Experience() {
+  return (
+    <>
+      <h3>Software Developer</h3>
+      <h4>Test123 Sdn Bhd</h4>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <h1>Steven Woh</h1>
+      <h1>Software developer</h1>
+      <Experience />
+    </>
+  )
+}
+
+export default App;
